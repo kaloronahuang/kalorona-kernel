8d9ff5bad483c180bacd5215b4443a4e2ef85c98
kaloronahuang <kaloronahuang@outlook.com>
2023-04-19 13:57:54 -0500

    kmem_object: bug fixed

--- kernel/memory/object.c
+++ kernel/memory/object.c
@@ -1,6 +1,7 @@
 // object.c
 #include <kmem.h>
 #include <signal.h>
+#include <console.h>
 #include <utilities/string.h>
 
 void *kmem_object_alloc(struct kmem_object_manager_struct *mgr)
@@ -21,7 +22,7 @@ void *kmem_object_alloc(struct kmem_object_manager_struct *mgr)
             break;
         }
         else
-            allocated_sum += blk->used * blk->object_size;
+            allocated_sum += blk->used * (blk->object_size + sizeof(struct list_node));
     if (!allocated)
     {
         // new block maybe;
@@ -82,15 +83,16 @@ struct kmem_object_block_struct *kmem_object_create_block(ulong page_order, size
     block->free_slots.nxt = block->allocated_slots.nxt = NULL;
     block->free_slots.prv = block->allocated_slots.prv = NULL;
     block->used = block->total = 0;
-    block->object_size = object_size;
+    block->object_size = BINARY_ROUND_UP(object_size, OBJECT_ALIGNMENT);
     block->page_count = 1ul << page_order;
     block->nxt = NULL;
     // create objects;
     struct list_node *previous = &(block->free_slots);
     ulong page_end = (ulong)block + (1ul << (page_order + PAGE_SHIFT));
+    size_t unit_size = sizeof(struct list_node) + block->object_size;
     for (ulong pos = BINARY_ROUND_UP((ulong)block + sizeof(struct kmem_object_block_struct), OBJECT_ALIGNMENT);
-         pos < page_end;
-         pos += sizeof(struct list_node) + object_size)
+         pos + unit_size <= page_end;
+         pos += unit_size)
     {
         struct list_node *cur = (struct list_node *)pos;
         previous->nxt = cur;
@@ -104,9 +106,8 @@ struct kmem_object_block_struct *kmem_object_create_block(ulong page_order, size
 struct kmem_object_manager_struct *kmem_object_create_manager(size_t object_size, const char *name)
 {
     struct kmem_object_manager_struct *new_mgr = kmem_object_alloc(&kmem.object_manager);
-
     strncpy(new_mgr->block_name, name, MAX_KMEM_OBJECT_BLOCK_NAME_LEN);
-    new_mgr->object_size = object_size;
+    new_mgr->object_size = BINARY_ROUND_UP(object_size, OBJECT_ALIGNMENT);
     new_mgr->blocks = NULL;
     new_mgr->nxt = NULL;
 
@@ -115,12 +116,14 @@ struct kmem_object_manager_struct *kmem_object_create_manager(size_t object_size
     new_mgr->nxt = kmem.object_manager.nxt;
     kmem.object_manager.nxt = new_mgr;
     spinlock_release(&kmem.object_manager_lock);
-
     return new_mgr;
 }
 
 void kmem_object_init(void)
 {
-    printf("[kmem_object_manager]initializing\n");
+    printf("[kmem_object]initializing\n");
+    strcpy(kmem.object_manager.block_name, "kmem_object_manager");
+    kmem.object_manager.object_size = BINARY_ROUND_UP(sizeof(struct kmem_object_manager_struct), OBJECT_ALIGNMENT);
     spinlock_init(&kmem.object_manager_lock, "object_manager_lock");
+    kmem_object_test();
 }
