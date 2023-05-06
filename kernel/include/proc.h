1032397fbb2141dae4b4979deb78c8b02c92a479
kaloronahuang <kaloronahuang@outlook.com>
2023-05-06 15:07:49 -0500

    proc: established basic code

--- kernel/include/proc.h
+++ kernel/include/proc.h
@@ -10,40 +10,54 @@
 #include <asm/hart.h>
 #include <utilities/algorithm.h>
 
-enum procstate
+enum proc_state
 {
-    UNUSED,
-    USED,
-    SLEEPING,
-    RUNNABLE,
-    RUNNING,
-    ZOMBIE
+    PROC_SLEEPING,
+    PROC_RUNNABLE,
+    PROC_RUNNING,
+    PROC_STOPPED,
+    PROC_ZOMBIE
 };
 
 struct proc_struct
 {
     struct spinlock lock;
 
-    int pid;
-    enum procstate state;
+    /* Protected by Local Lock - Begin */
+    uint pid;
+    enum proc_state state;
     int killed;
-    int exit_status;
+    int exit_state;
+    /* Protected by Local Lock - End */
 
     struct proc_struct *parent;
 
     void *stack_addr;
-    size_t mem_siz;
+    void *heap_addr;
     pagetable_t pgtbl;
     struct trapframe_struct *trapframe;
     struct context_struct context;
 };
 
-struct uspace
+struct proc_manager_struct
 {
-    /* list of struct proc_struct */
-    struct list_node proc_list;
+    struct spinlock lock;
 
+    /* Protected by Local Lock - Begin */
+    uint pid_counter;
+    /* Protected by Local Lock - End */
+
+    struct proc_struct *root_proc;
+
+    struct kmem_object_manager_struct *proc_object_manager;
 };
+
+extern struct proc_manager_struct proc_manager;
+
+struct proc_struct *proc_create();
+void proc_reap(struct proc_struct *p);
+
+void proc_extend_stack(struct proc_struct *p, int page_order);
 
 void proc_init(void);
 
--- kernel/proc/proc.c
+++ kernel/proc/proc.c
@@ -1,8 +1,70 @@
 // proc.c
 #include <proc.h>
 #include <console.h>
+#include <utilities/string.h>
+#include <asm/hart.h>
+#include <signal.h>
+
+struct proc_manager_struct proc_manager;
+
+struct proc_struct *proc_create()
+{
+    struct proc_struct *p = (struct proc_struct *)kmem_object_alloc(proc_manager.proc_object_manager);
+    if (p == NULL)
+        return NULL;
+    spinlock_init(&(p->lock), "proc_lock");
+
+    uint new_pid = 0;
+    spinlock_acquire(&(proc_manager.lock));
+    new_pid = ++proc_manager.pid_counter;
+    spinlock_release(&(proc_manager.lock));
+
+    spinlock_acquire(&(p->lock));
+    p->pid = new_pid;
+    p->state = PROC_RUNNABLE;
+    p->killed = 0;
+    p->exit_state = 0;
+    spinlock_release(&(p->lock));
+
+    p->parent = proc_manager.root_proc;
+    p->stack_addr = VA_USER_USER_HANDLER_BEGIN;
+    p->heap_addr = VA_USER_BEGIN;
+    p->pgtbl = vm_user_make_pagetable();
+    p->trapframe = (struct trapframe_struct *)kmem_alloc_pages(0);
+    memset(p->trapframe, 0, sizeof(struct trapframe_struct));
+    vm_mappages(p->pgtbl, VA_USER_TRAPFRAME_BEGIN, PMA_VA2PA(p->trapframe), sizeof(struct trapframe_struct), PTE_FLAG_R | PTE_FLAG_W);
+    memset(&(p->context), 0, sizeof(struct context_struct));
+}
+
+void proc_reap(struct proc_struct *p)
+{
+    // TODO: How to lock? How to disassemble children processes to the root process?
+    kmem_free((void *)p);
+}
+
+void proc_extend_stack(struct proc_struct *p, int page_order)
+{
+    if (p == NULL)
+        panic("proc_extend_stack - NULL proc");
+    ulong extended_pages = kmem_alloc_pages(page_order);
+    ulong size = 1ul << page_order;
+    p->stack_addr -= size;
+    vm_mappages(p->pgtbl, (ulong)p->stack_addr, PMA_VA2PA(extended_pages), size, PTE_FLAG_R | PTE_FLAG_W);
+}
+
+void proc_extend_heap(struct proc_struct *p, int page_order)
+{
+    if (p == NULL)
+        panic("proc_extend_stack - NULL proc");
+    ulong extended_pages = kmem_alloc_pages(page_order);
+    ulong size = 1ul << page_order;
+    vm_mappages(p->pgtbl, (ulong)p->heap_addr, PMA_VA2PA(extended_pages), size, PTE_FLAG_R | PTE_FLAG_W);
+    p->heap_addr += size;
+}
 
 void proc_init(void)
 {
-    printf("[proc]initializing user space");
+    printf("[proc]initializing user space\n");
+    spinlock_init(&(proc_manager.lock), "proc_manager_lock");
+    proc_manager.proc_object_manager = kmem_object_create_manager(sizeof(struct proc_struct), "proc_struct");
 }
